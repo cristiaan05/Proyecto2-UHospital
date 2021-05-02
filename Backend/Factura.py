0c48dcd3b154dc233300d4ec5392635aceaa4598
Cristian Hernández <chtello05@gmail.com>
2021-05-02 01:42:03 -0600

    Update Factura.py

--- Backend/Factura.py
+++ Backend/Factura.py
@@ -1,9 +1,8 @@
 class Factura:
-    def __init__(self,id,fecha,nombre,hora,doctor,costoOperacion,costoInter,total):
+    def __init__(self,id,fecha,nombre,doctor,costoOperacion,costoInter,total):
         self.id=id
         self.fecha=fecha
         self.nombre=nombre
-        self.hora=hora
         self.doctor=doctor
         self.costoOperacion=costoOperacion
         self.costoInter=costoInter
